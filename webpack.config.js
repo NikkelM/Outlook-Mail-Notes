fd0f2966e962cc7baea717fd7ec0da57e1e1585a
Nikkel Mollenhauer <nikkel.mollenhauer@student.hpi.de>
2023-07-11 18:18:00 +0200

    Removed boilerplate

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,6 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const urlDev = "https://localhost:3000/";
-// const urlProd = "https://www.contoso.com/"; // CHANGE THIS TO YOUR PRODUCTION DEPLOYMENT LOCATION
 
 async function getHttpsOptions() {
   const httpsOptions = await devCerts.getHttpsServerOptions();
