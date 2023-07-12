d8084fff5f450e23c8a4719bc1faf7381e8da8e8
Nikkel Mollenhauer <nikkel.mollenhauer@student.hpi.de>
2023-07-12 17:02:17 +0200

    Moved some code to separate functions, defined types

--- src/taskpane/taskpane.ts
+++ src/taskpane/taskpane.ts
@@ -1,67 +1,76 @@
 /* global document, Office */
 import Quill from "quill";
-var Delta = Quill.import('delta');
+var Delta = Quill.import("delta");
 
 let mailbox: Office.Mailbox;
 let settings: Office.RoamingSettings;
-let mailItem;
+let mailItem: string;
 // let conversation, sender;
 let quill: Quill;
 
 Office.onReady(async (info) => {
   if (info.host === Office.HostType.Outlook) {
-    // All options that should be displayed in the editor toolbar
-    var toolbarOptions = [
-      [{ size: ["small", false, "large", "huge"] }],
-      ["bold", "italic", "underline", "strike"],
-      ["link", "image"],
-      [{ color: [] }, { background: [] }],
-      [{ list: "ordered" }, { list: "bullet" }],
-    ];
+    setupQuill();
 
-    // Defines the Quill editor
-    quill = new Quill("#noteInput", {
-      modules: {
-        toolbar: toolbarOptions,
-      },
-      placeholder: "Jot down some notes here - your changes are automatically saved.\nUse the toolbar above to style your text!",
-      theme: "snow",
-    });
-
-    document.getElementById("savingNoteButton").onclick = saveNote;
+    // Set up references to the mailbox and the current item
     mailbox = Office.context.mailbox;
     settings = Office.context.roamingSettings;
     mailItem = mailbox.item.itemId;
     // conversation = mailbox.item.conversationId;
     // sender = mailbox.item.from.emailAddress;
 
-    // Load the existing note from storage
+    // Load a possible existing note from storage
     await displayExistingNote();
     // Start the autosave timer
     autosaveNote();
   }
 });
 
-async function displayExistingNote() {
+function setupQuill(): void {
+  // All options that should be displayed in the editor toolbar
+  var toolbarOptions = [
+    [{ size: ["small", false, "large", "huge"] }],
+    ["bold", "italic", "underline", "strike"],
+    ["link", "image"],
+    [{ color: [] }, { background: [] }],
+    [{ list: "ordered" }, { list: "bullet" }],
+  ];
+
+  // Defines the Quill editor
+  quill = new Quill("#noteInput", {
+    modules: {
+      toolbar: toolbarOptions,
+    },
+    placeholder:
+      "Jot down some notes here - your changes are automatically saved.\nUse the toolbar above to style your text!",
+    theme: "snow",
+  });
+}
+
+async function displayExistingNote(): Promise<void> {
   const note = await settings.get(mailItem);
   if (note) {
     quill.setContents(note);
   }
 }
 
-async function saveNote() {
+async function saveNote(): Promise<void> {
   const button: HTMLButtonElement = document.getElementById("savingNoteButton") as HTMLButtonElement;
   button.style.display = "inline-block";
 
   const note = quill.getContents();
+
+  // Save the note to storage
   settings.set(mailItem, note);
   settings.saveAsync();
+
+  // Hide the button after a timeout
   setTimeout(() => {
     button.style.display = "none";
   }, 1000);
 }
 
-let timeoutId;
+let autosaveTimeout: NodeJS.Timeout;
 
 function autosaveNote() {
   let accumulatedChanges = new Delta();
@@ -70,8 +79,8 @@ function autosaveNote() {
     accumulatedChanges = accumulatedChanges.compose(delta);
 
     // Changes are saved after a period of inactivity
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(function () {
+    clearTimeout(autosaveTimeout);
+    autosaveTimeout = setTimeout(function () {
       saveNote();
       accumulatedChanges = new Delta();
     }, 750);
