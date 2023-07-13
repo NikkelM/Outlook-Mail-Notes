8b2ac8a3ab024875a56c79b6cea2175cf4eecb55
Nikkel Mollenhauer <nikkel.mollenhauer@student.hpi.de>
2023-07-13 13:30:50 +0200

    Removed commands references

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,6 @@ module.exports = async (env, options) => {
     entry: {
       polyfill: ["core-js/stable", "regenerator-runtime/runtime"],
       taskpane: ["./src/taskpane/taskpane.ts", "./src/taskpane/taskpane.html"],
-      commands: "./src/commands/commands.ts",
     },
     output: {
       clean: true,
@@ -81,12 +80,7 @@ module.exports = async (env, options) => {
             },
           },
         ],
-      }),
-      new HtmlWebpackPlugin({
-        filename: "commands.html",
-        template: "./src/commands/commands.html",
-        chunks: ["polyfill", "commands"],
-      }),
+      })
     ],
     devServer: {
       headers: {
