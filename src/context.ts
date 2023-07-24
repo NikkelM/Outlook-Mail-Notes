703c5cfc109c7184ccd06f96653ce200e7205cf5
Nikkel Mollenhauer <nikkel.mollenhauer@student.hpi.de>
2023-07-24 17:07:39 +0200

    Refactored context button logic

--- src/context.ts
+++ src/context.ts
@@ -1,73 +1,83 @@
 // Contains logic to handle context buttons
 /* global document, Office */
-const emailContextButton = document.getElementById("emailContextButton");
-const senderContextButton = document.getElementById("senderContextButton");
-const conversationContextButton = document.getElementById("conversationContextButton");
-let activeContext = "mail";
 
-emailContextButton.addEventListener("click", () => {
-  // Set the active button
-  emailContextButton.classList.add("active");
-  senderContextButton.classList.remove("active");
-  conversationContextButton.classList.remove("active");
+import Quill from "quill";
+import { getIdentifiers, getSettings } from "./officeData";
 
-  // loadEmailNote();
-});
+const contextButtons = {
+  mail: document.getElementById("emailContextButton"),
+  sender: document.getElementById("senderContextButton"),
+  conversation: document.getElementById("conversationContextButton"),
+};
+let activeContext;
 
-senderContextButton.addEventListener("click", () => {
-  // Set the active button
-  emailContextButton.classList.remove("active");
-  senderContextButton.classList.add("active");
-  conversationContextButton.classList.remove("active");
-
-  // loadSenderNote();
-});
-
-conversationContextButton.addEventListener("click", () => {
-  // Set the active button
-  emailContextButton.classList.remove("active");
-  senderContextButton.classList.remove("active");
-  conversationContextButton.classList.add("active");
-
-  // loadConversationNote();
-});
+for (const [key, button] of Object.entries(contextButtons)) {
+  button.addEventListener("click", () => {
+    setActiveContext(key);
+    loadNoteForContext(key);
+  });
+}
 
 export function initContextButtons() {
   // Set the initial active button
-  emailContextButton.classList.add("active");
+  contextButtons.mail.classList.add("active");
+  activeContext = "mail";
 }
 
 export function setActiveContext(context: string) {
-  switch (context) {
-    case "mail":
-      emailContextButton.classList.add("active");
-      senderContextButton.classList.remove("active");
-      conversationContextButton.classList.remove("active");
-      activeContext = "mail";
-      break;
-    case "sender":
-      emailContextButton.classList.remove("active");
-      senderContextButton.classList.add("active");
-      conversationContextButton.classList.remove("active");
-      activeContext = "sender";
-      break;
-    case "conversation":
-      emailContextButton.classList.remove("active");
-      senderContextButton.classList.remove("active");
-      conversationContextButton.classList.add("active");
-      activeContext = "conversation";
-      break;
-    default:
-      throw new Error("Invalid context");
+  const button = contextButtons[context];
+  if (!button) {
+    throw new Error("Invalid context");
   }
+
+  for (const [key, value] of Object.entries(contextButtons)) {
+    if (key === context) {
+      value.classList.add("active");
+    } else {
+      value.classList.remove("active");
+    }
+  }
+
+  activeContext = context;
 }
 
 export function getActiveContext(): string {
   return activeContext;
 }
 
-// function loadEmailNote(mailId) {}
+export async function loadNoteForContext(
+  context: string,
+  quill?: Quill,
+  itemId?: string,
+  settings?: Office.RoamingSettings
+) {
+  if (!settings) {
+    settings = getSettings();
+  }
 
-// function loadSenderNote(senderId) {}
+  if (!quill) {
+    quill = Quill.find(document.getElementById("noteInput"));
+  }
 
-// function loadConversationNote(conversationId) {}
+  if (!itemId) {
+    const identifiers = getIdentifiers();
+    switch (context) {
+      case "mail":
+        itemId = identifiers.mailId;
+        break;
+      case "sender":
+        itemId = identifiers.senderId;
+        break;
+      case "conversation":
+        itemId = identifiers.conversationId;
+        break;
+      default:
+        throw new Error("Invalid context");
+    }
+  }
+
+  const allNotes = await settings.get("notes");
+
+  let noteContents = allNotes[itemId]?.noteContents ?? null;
+  quill.setContents(noteContents);
+}
