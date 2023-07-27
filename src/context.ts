2021838ef41f50269e98f64d8a96dd0f64968c37
Nikkel Mollenhauer <nikkel.mollenhauer@student.hpi.de>
2023-07-27 16:02:09 +0200

    Focus the editor on context change

--- src/context.ts
+++ src/context.ts
@@ -81,4 +81,6 @@ async function loadNoteForContext(context: string, quill?: Quill, itemId?: strin
 
   let noteContents = allNotes[itemId]?.noteContents ?? null;
   quill.setContents(noteContents);
+  // Focus the editor and insert the cursor at the end
+  quill.setSelection(quill.getLength(), 0);
 }
