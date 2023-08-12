5d86798e60421f42fcfdbd9ee1639d12eba060c5
Nikkel Mollenhauer <57323886+NikkelM@users.noreply.github.com>
2023-08-12 19:23:45 +0200

    Added a timestamp notice at the bottom of the editor (#58)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,10 +1,16 @@
 # Changelog
 
-## v1.2.2
+## v1.2.3
 
 <!--Releasenotes start-->
-- Updated the settings icon's file format to ensure cross-browser compatibility.
+- When opening the Add-In window, the most recently edited note is now opened.
+- Added a display for when the current note was last edited.
+- Fixed some layouting on the settings page.
 <!--Releasenotes end-->
+
+## v1.2.2
+
+- Updated the settings icon's file format to ensure cross-browser compatibility.
 
 ## v1.2.1
 
--- src/context.ts
+++ src/context.ts
@@ -5,6 +5,9 @@ import Quill from "quill";
 import { focusEditor } from "./editor";
 import { getIdentifiers, getSettings } from "./officeData";
 
+// Text element showing when the current note was last edited
+const lastEditedNotice: HTMLParagraphElement = document.getElementById("lastEditedNotice") as HTMLParagraphElement;
+
 const contextButtons = {
   mail: document.getElementById("emailContextButton"),
   sender: document.getElementById("senderContextButton"),
@@ -14,8 +17,8 @@ let activeContext;
 
 export function setupContextButtons(): void {
   for (const [key, button] of Object.entries(contextButtons)) {
-    button.addEventListener("click", () => {
-      switchToContext(key);
+    button.addEventListener("click", async () => {
+      await switchToContext(key);
     });
   }
 }
@@ -82,8 +85,49 @@ async function loadNoteForContext(context: string, quill?: Quill, itemId?: strin
 
   const allNotes = await settings.get("notes");
 
-  let noteContents = allNotes[itemId]?.noteContents ?? null;
+  const noteContents = allNotes[itemId]?.noteContents ?? null;
   quill.setContents(noteContents);
 
+  updateLastEditedNotice(itemId, allNotes);
+
   focusEditor();
+}
+
+export function updateLastEditedNotice(itemId: string, allNotes: any): void {
+  let lastEdited = allNotes[itemId]?.lastEdited ?? null;
+
+  if (lastEdited) {
+    const date: any = new Date(lastEdited);
+    const now: any = new Date();
+    const today: any = new Date();
+    const yesterday: any = new Date(today);
+    yesterday.setDate(today.getDate() - 1);
+
+    const minuteDiff = Math.floor((now - date) / (1000 * 60));
+    const hourDiff = Math.floor((now - date) / (1000 * 60 * 60));
+    const dayDiff = Math.floor((now - date) / (1000 * 60 * 60 * 24));
+
+    let formattedDate;
+    if (minuteDiff < 1) {
+      formattedDate = "Just now";
+    } else if (hourDiff === 0) {
+      formattedDate = "Less than an hour ago";
+    } else if (date.toDateString() === today.toDateString()) {
+      formattedDate = "Today";
+    } else if (date.toDateString() === yesterday.toDateString()) {
+      formattedDate = "Yesterday";
+    } else if (dayDiff < 7) {
+      formattedDate = date.toLocaleDateString(navigator.language, { weekday: "long", month: "long", day: "numeric" });
+    } else {
+      formattedDate = date.toLocaleDateString(navigator.language, {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      });
+    }
+
+    lastEditedNotice.innerHTML = `<i>Last edited: ${formattedDate}</i>`;
+  } else {
+    lastEditedNotice.innerText = "";
+  }
 }
